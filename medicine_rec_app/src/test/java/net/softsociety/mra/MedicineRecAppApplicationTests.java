0f1128dceefc7e8729996852d15135c35f8797f4
SES-2303\user <jeterunfeu@gmail.com>
2023-04-24 15:55:00 +0900

    initialization

---
+++ medicine_rec_app/src/test/java/net/softsociety/mra/MedicineRecAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.mra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineRecAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
