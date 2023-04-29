506eee5369f2d32a0a5bbc315d4167a725283f77
SES-2303\user <jeterunfeu@gmail.com>
2023-04-29 10:07:00 +0900

    로그인 기능 추가

---
+++ medicine_rec_app/src/main/java/net/softsociety/mra/vo/Member.java
@@ -0,0 +1,63 @@
+package net.softsociety.mra.vo;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member implements UserDetails {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -381255349641755611L;
+	
+	private String memberid;
+	private String memberpw;
+	private String membername;
+	private String email;
+	private String phone;
+	private String address;
+	boolean enabled;
+	String rolename;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		return this.getMemberpw();
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.getMemberid();
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+}
