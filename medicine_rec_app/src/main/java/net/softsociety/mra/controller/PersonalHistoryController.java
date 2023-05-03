66d722d06f93b2aaea6a15ae46ae3b4b98880632
SES-2303\user <jeterunfeu@gmail.com>
2023-05-03 22:58:00 +0900

    내역 controller 수정

--- medicine_rec_app/src/main/java/net/softsociety/mra/controller/PersonalHistoryController.java
+++ medicine_rec_app/src/main/java/net/softsociety/mra/controller/PersonalHistoryController.java
@@ -40,9 +40,6 @@ public class PersonalHistoryController {
 	@GetMapping()
 	public PageNavigator selectAll(@AuthenticationPrincipal UserDetails user, @RequestParam(name="page", defaultValue="1") int page, History history){
 		MediMember person = service.selectOneById(user.getUsername());
-		if(history.getMembernum() != person.getMembernum()) {
-			return null;
-		}
 		history.setMembernum(person.getMembernum());
 		PageNavigator result = service.getPageNavigator(pagePerGroup, countPerPage, page, history);
 		List<History> data = service.selectAll(result, history);
