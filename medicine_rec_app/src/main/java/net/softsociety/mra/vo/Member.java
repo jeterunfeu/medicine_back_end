7a7e50c2c5653ddcc9c299376d0d1b9cd082223e
SES-2303\user <jeterunfeu@gmail.com>
2023-05-03 20:27:46 +0900

    Member vo 롬복 추가

--- medicine_rec_app/src/main/java/net/softsociety/mra/vo/Member.java
+++ medicine_rec_app/src/main/java/net/softsociety/mra/vo/Member.java
@@ -1,5 +1,12 @@
 package net.softsociety.mra.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	int membernum;
 	String memberid;
